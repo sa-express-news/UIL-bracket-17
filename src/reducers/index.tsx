b0bf1433be6937af36f849d127dec2a675ce73d2
Kia Farhang <kfarhang0@gmail.com>
2017-10-31 17:38:00 -0500

    Wrote a reducer for updating a node that I'm really not proud of

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,10 +1,37 @@
-import { StoreState } from '../types';
+import { update, cloneDeep } from 'lodash';
+
+import { StoreState, Bracket, Game } from '../types';
 import * as constants from '../constants';
-import { Action, UpdateBracketIndex } from '../actions';
+import { Action, UpdateBracketIndex, UpdateNode } from '../actions';
 
 
 export const bracketIndex = (state: number, action: UpdateBracketIndex): number => {
     return action.index;
+}
+
+export const nodeUpdate = (state: Bracket, action: UpdateNode): Bracket => {
+    if (state.champion.id === action.id) {
+        return Object.assign({}, state, { champion: action.node });
+    } else {
+
+        const containsMatchingNode = (game: Game): boolean => {
+            return game.nodes.find(node => node.id === action.id) !== undefined;
+        }
+
+        const indexOfGameContainingNode = state.games.findIndex(containsMatchingNode);
+
+        if (indexOfGameContainingNode < 0) {
+            return state;
+        } else {
+            const newGames = update(cloneDeep(state.games), `${indexOfGameContainingNode}`, function (game: Game) {
+                const indexOfNode = game.nodes.findIndex(node => node.id === action.id);
+                const gameCopy = cloneDeep(game);
+                gameCopy.nodes[indexOfNode] = action.node;
+                return gameCopy;
+            });
+            return Object.assign({}, state, { games: newGames });
+        }
+    }
 }
 
 export const bracketApp = (state: StoreState, action: Action): StoreState => {
