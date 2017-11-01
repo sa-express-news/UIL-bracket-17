96350b69eed17685a10a4ef3ba2114a57ec063bb
Kia Farhang <kfarhang0@gmail.com>
2017-11-01 15:08:58 -0500

    Retooled App component to contain the redux container component for brackets

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
+import BracketContainer from '../BracketContainer';
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <BracketContainer />
+    </div>
+  )
 }
 
 export default App;
