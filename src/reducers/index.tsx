ed537bb2a3605169788516bd143f700eac107cb0
Kia Farhang <kfarhang0@gmail.com>
2017-11-01 10:13:35 -0500

    Defined initial store state in reducers/index.tsx

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -4,6 +4,14 @@ import { StoreState, Bracket, Game } from '../types';
 import * as constants from '../constants';
 import { Action, UpdateBracketIndex, UpdateNode } from '../actions';
 
+import startingBrackets from './startingBrackets';
+
+const initialState: StoreState = {
+    activeBracketIndex: 0,
+    userBrackets: startingBrackets,
+    canonicalBrackets: startingBrackets
+};
+
 
 export const bracketIndex = (state: number, action: UpdateBracketIndex): number => {
     return action.index;
@@ -35,10 +43,15 @@ export const nodeUpdate = (state: Bracket, action: UpdateNode): Bracket => {
 }
 
 export const bracketApp = (state: StoreState, action: Action): StoreState => {
+    if (!state) return initialState;
     switch (action.type) {
         case constants.UPDATE_BRACKET_INDEX:
             return Object.assign({}, state, {
                 activeBracketIndex: bracketIndex(state.activeBracketIndex, action as UpdateBracketIndex)
+            });
+        case constants.UPDATE_NODE:
+            return Object.assign({}, state, {
+                userBrackets: nodeUpdate(state.userBrackets[state.activeBracketIndex], action as UpdateNode)
             });
         default:
             return state;
