806a866df060ee05147727fc78bbf5a45785f075
Kia Farhang <kfarhang0@gmail.com>
2017-11-01 15:09:16 -0500

    Added Redux provider to app entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { bracketApp } from './reducers';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore(bracketApp);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
