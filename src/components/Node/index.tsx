e196f74e7f4730208b4a6e4628ad1b49cffd5abb
Kia Farhang <kfarhang0@gmail.com>
2017-11-02 11:28:11 -0500

    Added ability to grab updateNode function prop to Game, Node components

--- src/components/Node/index.tsx
+++ src/components/Node/index.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import { Node as NodeProps } from '../../types';
 
+import { updateNode } from '../../actions';
+
 import Team from '../Team';
 
-const Node = ({ id, team, childID, parentIDs, legalityFunction }: NodeProps) => {
+const Node = ({ id, team, childID, parentIDs, legalityFunction, updateNodeFunction }: NodeProps) => {
+
+    const handleClick = (event: any): void => {
+        event.preventDefault();
+        const updateAction = updateNode(childID, { id: childID, team: team });
+        updateNodeFunction(updateAction);
+    }
+
+
     return (
-        <div className="Node">
+        <div className="Node" onClick={handleClick}>
             {team !== null ? <Team name={team.name} colors={team.colors} logo={team.logo} /> : null}
         </div>
     )
