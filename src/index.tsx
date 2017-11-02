0f1aab8edaa43a707524721c0dff9c82ed86530d
Kia Farhang <kfarhang0@gmail.com>
2017-11-02 13:53:00 -0500

    Installed and added Redux dev tools package

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { bracketApp } from './reducers';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const store = createStore(bracketApp);
+const store = createStore(bracketApp, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
