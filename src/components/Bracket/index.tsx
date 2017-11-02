bf3894cc0c294396ef05c429f2e9ac5cfda77145
Kia Farhang <kfarhang0@gmail.com>
2017-11-02 11:28:52 -0500

    Updated Bracket component to pass dispatch update node function down to Node, Game

--- src/components/Bracket/index.tsx
+++ src/components/Bracket/index.tsx
@@ -4,23 +4,30 @@ import { Bracket as BracketProps, Team } from '../../types';
 import Game from '../Game';
 import Node from '../Node';
 
+import { UpdateNode, updateNode } from '../../actions'
+
 import * as bracketFunctions from '../../data-structures/bracket';
 
 const Bracket = (bracket: BracketProps) => {
-    const { name, games, champion, identifier } = bracket;
-    const gameComponents = games.map((game, index) => {
-        return <Game location={game.location} time={game.time} nodes={game.nodes} legalityFunctionForNodes={isNodeUpdateLegal} key={index} />
-    });
+    const { name, games, champion, identifier, dispatch } = bracket;
 
     const isNodeUpdateLegal = (id: number, team: Team): boolean => {
         return bracketFunctions.isTeamUpdateLegal(bracket, id, team);
     }
 
+    const dispatchNodeUpdate = (action: UpdateNode): Function => {
+        return dispatch(action);
+    }
+
+    const gameComponents = games.map((game, index) => {
+        return <Game location={game.location} time={game.time} nodes={game.nodes} legalityFunctionForNodes={isNodeUpdateLegal} updateNodeFunction={dispatchNodeUpdate} key={index} />
+    });
+
     return (
         <div className="Bracket">
             <h3>{name}</h3>
             {gameComponents}
-            <Node id={champion.id} team={champion.team} parentIDs={champion.parentIDs} legalityFunction={isNodeUpdateLegal} />
+            <Node id={champion.id} team={champion.team} parentIDs={champion.parentIDs} legalityFunction={isNodeUpdateLegal} updateNodeFunction={dispatchNodeUpdate} />
         </div>
     )
 }
