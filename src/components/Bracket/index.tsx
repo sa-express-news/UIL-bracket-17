511243292582bf8fffaf4f93349c66595964c80e
Kia Farhang <kfarhang0@gmail.com>
2017-11-02 09:33:55 -0500

    Updated Bracket component to pass a legality function to its games and nodes

--- src/components/Bracket/index.tsx
+++ src/components/Bracket/index.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
-import { Bracket as BracketProps } from '../../types';
+import { Bracket as BracketProps, Team } from '../../types';
 
 import Game from '../Game';
 import Node from '../Node';
 
-const Bracket = ({ name, games, champion, identifier }: BracketProps) => {
+import * as bracketFunctions from '../../data-structures/bracket';
+
+const Bracket = (bracket: BracketProps) => {
+    const { name, games, champion, identifier } = bracket;
     const gameComponents = games.map((game, index) => {
-        return <Game location={game.location} time={game.time} nodes={game.nodes} key={index} />
+        return <Game location={game.location} time={game.time} nodes={game.nodes} legalityFunctionForNodes={isNodeUpdateLegal} key={index} />
     });
+
+    const isNodeUpdateLegal = (id: number, team: Team): boolean => {
+        return bracketFunctions.isTeamUpdateLegal(bracket, id, team);
+    }
 
     return (
         <div className="Bracket">
             <h3>{name}</h3>
             {gameComponents}
-            <Node id={champion.id} team={champion.team} parentIDs={champion.parentIDs} />
+            <Node id={champion.id} team={champion.team} parentIDs={champion.parentIDs} legalityFunction={isNodeUpdateLegal} />
         </div>
     )
 }
