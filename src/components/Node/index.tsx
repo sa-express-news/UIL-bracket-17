6be4582e777218ecb42e685470ecc4f2d98130b1
Kia Farhang <kfarhang0@gmail.com>
2017-11-04 10:40:04 -0500

    Some reformatting in Node component to please tslint

--- src/components/Node/index.tsx
+++ src/components/Node/index.tsx
@@ -10,16 +10,15 @@ const Node = ({ id, team, childID, parentIDs, legalityFunction, updateNodeFuncti
     const handleClick = (event: any): void => {
         event.preventDefault();
         const updateAction = updateNode(id, team);
-        if (updateNodeFunction) updateNodeFunction(updateAction);
+        if (updateNodeFunction) { updateNodeFunction(updateAction); }
         updateGameIndexFunction();
-    }
-
+    };
 
     return (
         <div className="Node" onClick={handleClick}>
             {team !== null ? <Team name={team.name} colors={team.colors} logo={team.logo} /> : null}
         </div>
-    )
-}
+    );
+};
 
 export default Node;
