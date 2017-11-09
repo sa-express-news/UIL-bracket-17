1af576fdc41711fcec889e480f74be072a660a08
Kia Farhang <kfarhang0@gmail.com>
2017-11-09 11:17:33 -0600

    Bring Redux-connected Notification component into the top-level App component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import NotificationContainer from '../NotificationContainer';
 import BracketContainer from '../BracketContainer';
 
 const App = () => {
   return (
     <div>
+      <NotificationContainer />
       <BracketContainer />
     </div>
   )
