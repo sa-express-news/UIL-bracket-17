d37bf4ea30410fe5849fd357fa4522675c7253b8
Kia Farhang <kfarhang0@gmail.com>
2017-11-13 16:12:20 -0600

    Update root project URL and add share assets like favicon, OG meta content, etc

--- src/routerHistory.ts
+++ src/routerHistory.ts
@@ -1,7 +1,7 @@
 import { createBrowserHistory, BrowserHistoryBuildOptions } from 'history';
 
 const historyOptions: BrowserHistoryBuildOptions = {
-    basename: '/brackets/uiw-2017-bracket'
+    basename: '/brackets/uil-2017-bracket'
 };
 
 export default createBrowserHistory(historyOptions);
