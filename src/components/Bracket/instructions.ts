fd87bcb2b5db5d1588f0bbf83f5825d456252cc7
Kia Farhang <kfarhang0@gmail.com>
2017-11-14 17:39:23 -0600

    Amend instructions to mention the brackets are for Region 4

--- src/components/Bracket/instructions.ts
+++ src/components/Bracket/instructions.ts
@@ -1,5 +1,5 @@
 const instructions: string[] = [
-    `Build a bracket for Class 6A and 5A high school football playoffs and follow along as the games progress.`,
+    `Build a bracket for Region 4 Class 6A and 5A high school football playoffs and follow along as the games progress.`,
     `Start by selecting the class and division you want to create a bracket for from the dropdown menu.`,
     `Once you’ve completed your bracket(s), fill in your email and hit “save.” Then, bookmark the URL and come back anytime to see how you’re doing. You have until Thursday at 7 p.m. to adjust your bracket. Good luck!`
 ];
