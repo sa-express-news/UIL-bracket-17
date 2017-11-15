130282f8af884e502920b2b0a5f470f31a21c5c8
Kia Farhang <kfarhang0@gmail.com>
2017-11-15 11:48:54 -0600

    Update instructions to explain how incorrect brackets will be marked

--- src/components/Bracket/instructions.ts
+++ src/components/Bracket/instructions.ts
@@ -1,7 +1,8 @@
 const instructions: string[] = [
     `Build a bracket for Region 4 Class 6A and 5A high school football playoffs and follow along as the games progress.`,
     `Start by selecting the class and division you want to create a bracket for from the dropdown menu.`,
-    `Once you’ve completed your bracket(s), fill in your email and hit “save.” Then, bookmark the URL and come back anytime to see how you’re doing. You have until Thursday at 7 p.m. to adjust your bracket. Good luck!`
+    `Once you’ve completed your bracket(s), fill in your email and hit “save.” Then, bookmark the URL and come back anytime to see how you’re doing. You'll see red borders around any picks you got wrong.`,
+    `You have until Thursday at 7 p.m.to adjust your bracket.Good luck!`
 ];
 
 export const mobileNote = `You'll see one matchup at a time on the screen. Swipe left if you change your mind and want to move back.`;
